b13e44ccf4ab070a1808bcdb8137cf63c7bedfe7
Mady-T <adithya5463sajith@gmail.com>
2024-01-28 00:59:30 +0530

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+//    if (num <= 2) return 1;
+    let currNum = 1;
+    let prevNum = 1;
+    num -= 2;
+    while (num-- > 0) {
+        let tempNum = currNum;
+        currNum += prevNum;
+        prevNum = tempNum;
+    }
+    return currNum;
 };
 
 // Do not edit below this line
