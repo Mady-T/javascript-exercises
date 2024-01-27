9481212466296740a142358534ad7295d2585411
Mady-T <adithya5463sajith@gmail.com>
2024-01-28 00:37:18 +0530

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let strArray = string.toLowerCase().split("");
+    strArray = strArray.filter((value) => /\w/.test(value));
+    string = strArray.join("");
+    return string === strArray.reverse().join("");
 };
 
 // Do not edit below this line
