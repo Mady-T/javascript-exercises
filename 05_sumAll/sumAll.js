b09d8d57ff727fb9d1b6f246d726bca7db4fa4bc
Mady-T <adithya5463sajith@gmail.com>
2024-01-27 23:27:55 +0530

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(lowerNum, upperNum) {
+    if (typeof(lowerNum) !== "number" || typeof(upperNum) !== "number") {
+        return "ERROR";
+    }
+    if (upperNum < lowerNum) {
+        [upperNum, lowerNum] = [lowerNum, upperNum];
+    }
+    if (lowerNum < 0) return "ERROR";
+    let sum = lowerNum++;
+    while (lowerNum <= upperNum) {
+        sum += lowerNum++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
