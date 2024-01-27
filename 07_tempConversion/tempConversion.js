26d4f10b337ba6d6e91f5d8352d28037d7fae213
Mady-T <adithya5463sajith@gmail.com>
2024-01-27 23:57:55 +0530

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFarenheit) {
+  let degreesCelsius = (degreesFarenheit - 32) * 5/9;
+  return +degreesCelsius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let degreesFarenheit = (degreesCelsius * 9/5) + 32;
+  return +degreesFarenheit.toFixed(1);
 };
 
 // Do not edit below this line
