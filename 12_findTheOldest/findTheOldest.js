3c9c550b1c26c14d629cd9748868fe6df9a8d74e
Mady-T <adithya5463sajith@gmail.com>
2024-01-28 01:41:12 +0530

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    const YEAR = 1000*60*60*24*365.25;
+    const ageArray = peopleArray.map((person) => {
+        return ((person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth);
+    });
+    const maxAge = Math.max.apply(null,  ageArray);
+    const maxAgeIndex =  ageArray.indexOf(maxAge);
+    return peopleArray[maxAgeIndex];
 };
 
 // Do not edit below this line
