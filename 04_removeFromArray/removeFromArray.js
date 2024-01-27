e414b7762ae49fc98d190122d12c3f5f1825510c
Mady-T <adithya5463sajith@gmail.com>
2024-01-27 23:00:23 +0530

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elems) {
+    for (element of elems) {
+        let currIndex = array.indexOf(element);
+        while (currIndex !== -1) {
+            array.splice(currIndex, 1);
+            currIndex = array.indexOf(element, currIndex);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
